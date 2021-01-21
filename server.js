b40365b90f2b9eb3a4bb9222f0bd45cc01b670de
David Onuh <Davidonuh1@gmail.com>
2021-01-22 00:38:34 +0100

    rewrite. add express server base

--- server.js
+++ server.js
@@ -1,43 +1,19 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const movie_name = "shooter"
-const url = `https://www.rottentomatoes.com/m/${movie_name}`
+const http = require('http');
+const express = require('express');
+const { env } = require('process');
+
+const app = express();
+
+const hostname = 'localhost';
+const port = env.PORT || 3000;
+
+// init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API RUNNING!'));
 
 
-axios(url)
-.then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const data = [];
-    const main_container = $("#main_container")
-    const meta_data = main_container.find('ul[class="content-meta info"] > li')
-    //const meta_data_json = []
-    
-    //.cache @param RTRatings => RottenTomatoes Ratings
-    //const RTRatings = []
+app.listen(port, hostname, () =>{
+    console.log(`Server running at ${hostname}:${port}`)
+});
 
-    const RTRatings = {
-        'avg_rating': main_container.find('span[class="js-audience-score-info"]').text(),
-        'ratings_count': main_container.find('div[class="score_details__big-text js-audience-score-info"]').text() 
-    }
-
-    const meta_data_json = {
-        'pg' : String($(meta_data[0]).find(".meta-value").text()).trim(),
-        'genre' : String($(meta_data[1]).find(".meta-value").text()).trim(),
-        'director' : String($(meta_data[3]).find(".meta-value").text()).trim(),
-        'producer' : String($(meta_data[4]).find(".meta-value").text()).trim(),
-        'releaseDate' : String($(meta_data[5]).find(".meta-value").text()).trim(),
-        'boxOffice' : String($(meta_data[7]).find(".meta-value").text()).trim()
-        };
-    
-    data.push({
-        'name': movie_name,
-        'synopsis': String(main_container.find('#movieSynopsis').text()).trim(),
-        'metaContent' : meta_data_json,
-        'ratings': RTRatings
-
-    });
-
-    console.log(data);
-})
-.catch(console.error);

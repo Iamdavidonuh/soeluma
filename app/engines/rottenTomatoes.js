c7d9c56a2a6d262d8c3823d03e630a151419091e
David Onuh <Davidonuh1@gmail.com>
2021-01-22 08:56:27 +0100

    WIP: build rottenTomatoes engine to parse data

---
+++ app/engines/rottenTomatoes.js
@@ -0,0 +1,76 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { normalizeQueryParam } = require('../libs/util');
+
+function buildMovieDetailsUrl(query){
+    const base_url = `https://www.rottentomatoes.com/api/private/v1.0/movies/${query}`
+    return base_url
+}
+
+async function getMovieData(query){
+    const cleanedUrl = buildMovieDetailsUrl(normalizeQueryParam(query));
+    return axios(cleanedUrl)
+}
+
+function alternateMovieParse(query){
+
+    query = normalizeQueryParam(query)
+    const url = `https://www.rottentomatoes.com/m/${query}`
+    const data = [];
+    
+    axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        
+        const main_container = $("#main_container")
+        const meta_data = main_container.find('ul[class="content-meta info"] > li')
+        
+        //@param RTRatings => RottenTomatoes Ratings
+        const RTRatings = {
+            'avg_rating': main_container.find('span[class="js-audience-score-info"]').text(),
+            'ratings_count': main_container.find('div[class="score_details__big-text js-audience-score-info"]').text() 
+        }
+
+        const meta_data_json = {
+            'pg' : String($(meta_data[0]).find(".meta-value").text()).trim(),
+            'genre' : String($(meta_data[1]).find(".meta-value").text()).trim(),
+            'director' : String($(meta_data[3]).find(".meta-value").text()).trim(),
+            'producer' : String($(meta_data[4]).find(".meta-value").text()).trim(),
+            'releaseDate' : String($(meta_data[5]).find(".meta-value").text()).trim(),
+            'boxOffice' : String($(meta_data[7]).find(".meta-value").text()).trim()
+            };
+        
+        data.push({
+            'name': movie_name,
+            'synopsis': String(main_container.find('#movieSynopsis').text()).trim(),
+            'metaContent' : meta_data_json,
+            'ratings': RTRatings
+
+        });
+    });
+    return data;
+}
+
+
+const parseMovieDataRT = movieJson => {
+    /**
+     * parses the json data gotten from RT and returns necessary information
+     * 
+     */
+    const dataDict = [];
+    dataDict.push({
+        'title': movieJson.title,
+        'studio': movieJson.studio,
+        'avgRatings': movieJson.ratingSummary.allCritics.averageRating,
+        'consensus': movieJson.ratingSummary.consensus,
+        'movie_id': movieJson.id,
+        'synopsis': movieJson.synopsis
+    });
+
+    return dataDict;
+}
+
+exports.getMovieData = getMovieData;
+exports.alternateMovieParse = alternateMovieParse;
+exports.parseMovieDataRT = parseMovieDataRT;

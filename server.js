4f0351965341fb937cba33458f7f88195a73dbe1
David Onuh <Davidonuh1@gmail.com>
2021-01-27 15:23:55 +0100

    conditional db connection based on env variable. connect redis cache

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const { port } = require('./app/config/config');
+const { port, redisClient, useDocker } = require('./app/config/config');
 const { connectDBDocker, connectDBLocal } = require('./app/libs/db')
 const app = require('./app/app.js');
 
+
 //connect database
-//connectDBLocal()
+
+(useDocker===true) ? connectDBDocker() : connectDBLocal();
 
 const hostname = '0.0.0.0';
 
+// log error if any occurs
+redisClient.on("error", (err) => {
+    console.log(err);
+});
 
 app.listen(port, hostname, () =>{
     console.log(`Server running at ${hostname}:${port}`)

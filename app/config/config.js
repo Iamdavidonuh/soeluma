d2c7a7c7aa8c5eff892ba6cdf02b8c5b7b23e891
David Onuh <Davidonuh1@gmail.com>
2021-01-27 16:58:52 +0100

    promisify redis get

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,9 @@
 const redis = require('redis');
 const redisPort = 6379;
 const client = redis.createClient(redisPort);
+const { promisify } = require("util");
 
+const get = promisify(client.get).bind(client);
 module.exports =  {
 
     NODE_ENV:process.env.NODE_ENV,
@@ -9,5 +11,6 @@ module.exports =  {
     mongoDBLocalUri: process.env.MONGODB_URI_LOCAL,
     mongoDBDockerUri: process.env.MONGODB_URI_DOCKER,
     redisClient: client,
+    get: get,
     useDocker: process.env.USE_DOCKER
 };

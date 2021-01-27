073989898015225e9bcd2ed7fd29b2bf4afb02e3
David Onuh <Davidonuh1@gmail.com>
2021-01-27 15:22:35 +0100

    add redis and docker env variable to config

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,6 @@
-
+const redis = require('redis');
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
 
 module.exports =  {
 
@@ -6,4 +8,6 @@ module.exports =  {
     port:process.env.PORT,
     mongoDBLocalUri: process.env.MONGODB_URI_LOCAL,
     mongoDBDockerUri: process.env.MONGODB_URI_DOCKER,
+    redisClient: client,
+    useDocker: process.env.USE_DOCKER
 };

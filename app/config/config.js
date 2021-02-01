1f1db99225be381afa5cfc5189c15591a9f92234
David Onuh <Davidonuh1@gmail.com>
2021-02-01 15:44:03 +0100

    changed client declearation from let to var

--- app/config/config.js
+++ app/config/config.js
@@ -4,15 +4,14 @@ const { promisify } = require("util");
 if (process.env.REDIS_CLOUD_URL){
 
     var rtg = require('url').parse(process.env.REDIS_CLOUD_URL);
-    let client = redis.createClient(rtg.port, rtg.hostname);
+    var client = redis.createClient(rtg.port, rtg.hostname);
     client.auth(rtg.auth.split(":")[1]);
 
 }else{
-    let client = redis.createClient(process.env.REDIS_PORT);
+    var client = redis.createClient(process.env.REDIS_PORT);
 }
 //promisify redis get method
 const redis_get = promisify(client.get).bind(client);
-
 
 
 module.exports =  {
